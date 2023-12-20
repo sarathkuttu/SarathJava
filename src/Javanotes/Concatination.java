667bf5e5c97a34cc21831a86eee7d3356d1c5bd3
sarathkuttu <srthtv1991@gmail.com>
2023-12-21 00:00:35 +0530

    Dec 21st

---
+++ src/Javanotes/Concatination.java
@@ -0,0 +1,18 @@
+package src.Javanotes;
+
+public class Concatination {
+
+    public static void main(String[] args) {
+
+
+        int a = 100;
+
+        int b = 50;
+
+        System.out.println(a + "SARATH" + b);
+
+        System.out.println(a + b + "SARATH");
+
+        System.out.println("SARATH" + b + a);
+    }
+}
