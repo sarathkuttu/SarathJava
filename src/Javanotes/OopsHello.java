70cf2b84dda675c73d6db9552002a9fe8158316a
sarathkuttu <srthtv1991@gmail.com>
2024-01-03 17:15:06 +0530

    " Jan 3rd OOPS"

---
+++ src/Javanotes/OopsHello.java
@@ -0,0 +1,21 @@
+package src.Javanotes;
+
+public class OopsHello {
+
+    public static void main(String[] args) {
+
+        Hello4();
+        Hello4();
+
+
+    }
+
+    static void Hello4() {
+        for (int i = 0; i <= 4; i++) {
+
+            System.out.println("HELLO");
+
+        }
+
+    }
+}
