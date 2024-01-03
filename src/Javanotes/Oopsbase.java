eb7a903dcd284abcdf312a79becd95ad94510d98
sarathkuttu <srthtv1991@gmail.com>
2024-01-03 16:47:52 +0530

    OOPS

---
+++ src/Javanotes/Oopsbase.java
@@ -0,0 +1,18 @@
+package src.Javanotes;
+
+public class Oopsbase {
+
+    public static void main(String[] args) {
+
+        int add=sum(2,5,10);
+
+        System.out.println(add);
+
+
+    }
+
+    static int sum(int a, int b, int c) {
+
+        return a + b + c;
+    }
+}
