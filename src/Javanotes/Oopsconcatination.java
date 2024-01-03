2da2c3c81c5d082f2810e648ff89196756f2241d
sarathkuttu <srthtv1991@gmail.com>
2024-01-03 17:44:56 +0530

    " Jan 3rd OOPS"

---
+++ src/Javanotes/Oopsconcatination.java
@@ -0,0 +1,20 @@
+package src.Javanotes;
+
+public class Oopsconcatination {
+
+    public static void main(String[] args) {
+
+        String z = name("SARATH");
+
+        System.out.println(z);
+
+
+    }
+
+    static String name(String a) {
+
+        return a + " KUTTU";
+    }
+
+
+}
