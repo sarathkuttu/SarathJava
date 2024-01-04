b21e91b2a0e0cf9ecb5cde3b16af182eb4679cda
sarathkuttu <srthtv1991@gmail.com>
2024-01-04 12:14:36 +0530

    " Jan 4th ArrayD"

---
+++ src/Javanotes/DiagonalArray2.java
@@ -0,0 +1,33 @@
+package src.Javanotes;
+
+public class DiagonalArray2 {
+
+    public static void main(String[] args) {
+
+        int array[][] = new int[3][3];
+
+        array[0][0] = 10;
+        array[0][1] = 122;
+        array[0][2] = 15;
+        array[1][0] = 9;
+        array[1][1] = 2;
+        array[1][2] = 115;
+        array[2][0] = 110;
+        array[2][1] = 22;
+        array[2][2] = 5;
+
+        for (int i = 0; i < array.length; i++) {
+
+            for (int j = 0; j < array.length; j++) {
+
+                if (i == j) {
+
+                    System.out.println(array[i][j]);
+
+                }
+
+            }
+
+        }
+    }
+}
