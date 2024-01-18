3b286b0f2bfcdaee6bf8194976150762126d22f6
keerthanaaravind <keerthanaaravind8@gmail.com>
2024-01-18 19:40:33 +0530

    " Jan 18th Method Overriding"

---
+++ src/OopsNotes/Animal_Dog_Method_Overriding.java
@@ -0,0 +1,11 @@
+package src.OopsNotes;
+
+public class Animal_Dog_Method_Overriding extends Animal_Method_Overriding {
+
+    public void Sound() {
+
+        System.out.println("BARK");
+    }
+
+
+}
---
+++ src/OopsNotes/Animal_Method_Overriding.java
@@ -0,0 +1,11 @@
+package src.OopsNotes;
+
+public class Animal_Method_Overriding {
+
+    public void Sound() {
+
+        System.out.println("Any Kind of Sound");
+
+
+    }
+}
