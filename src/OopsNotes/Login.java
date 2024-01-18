baafa976c73405e8cbcfdf01c0301caf5b0f2900
keerthanaaravind <keerthanaaravind8@gmail.com>
2024-01-18 18:53:47 +0530

    " Jan 18th"

---
+++ src/OopsNotes/Login.java
@@ -0,0 +1,39 @@
+package src.OopsNotes;
+
+import java.util.Locale;
+
+
+public class Login {
+
+    public static void main(String[] args) {
+
+        Log access = new Log("admin", "admin");
+
+        access.isLoggedin("admin", "admin");
+    }
+
+    static class Log {
+        public String username;
+        public String password;
+
+        public Log(String username, String password) {
+
+            this.username = username;
+            this.password = password;
+        }
+
+        public boolean isLoggedin(String username, String password) {
+
+            if (this.username.toLowerCase().equals(username) && this.password.toLowerCase().equals(password)) {
+
+                System.out.println("Logged");
+                return true;
+            } else {
+
+                System.out.println("Not LOgged");
+                return false;
+            }
+
+        }
+    }
+}
---
+++ src/OopsNotes/car_Constructor2.java
@@ -0,0 +1,11 @@
+package src.OopsNotes;
+
+public class car_Constructor2 {
+
+    public static void main(String[] args) {
+
+        Car_Constructor car = new Car_Constructor("Honda", "Eclass", 2019);
+
+        car.print();
+    }
+}
