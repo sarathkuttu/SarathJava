97ecf2d828d60bb35e4bd7aef474d36a57e40f6c
keerthanaaravind <keerthanaaravind8@gmail.com>
2024-01-18 19:26:54 +0530

    " Jan 18th Method Overloading"

---
+++ src/OopsNotes/Method_Overloading.java
@@ -0,0 +1,24 @@
+package src.OopsNotes;
+
+public class Method_Overloading {
+
+    public int add(int a, int b) {
+
+        return a + b;
+    }
+
+    public double add(double a, double b) {
+
+        return a + b;
+    }
+
+    public String add(String a, String b) {
+
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+
+        return a + b + c;
+    }
+}
