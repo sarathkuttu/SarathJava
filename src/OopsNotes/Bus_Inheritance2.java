facb85041088f8ac7df380150502aa4346e42f87
keerthanaaravind <keerthanaaravind8@gmail.com>
2024-01-18 19:13:37 +0530

    " Jan 18th Inheritance"

---
+++ src/OopsNotes/Bus_Inheritance2.java
@@ -0,0 +1,19 @@
+package src.OopsNotes;
+
+public class Bus_Inheritance2 extends Bus_Inheritance {
+
+    int Doors;
+
+    public Bus_Inheritance2(String Bus_name, String Bus_model, int Bus_Doors) {
+        super(Bus_name, Bus_model);
+
+        this.Doors = Bus_Doors;
+    }
+
+    public void DisplayBus() {
+
+        Display();
+
+        System.out.println("Bus door number are " + this.Doors);
+    }
+}
