f4b3567a2976d99bc03a832223ffe383de0249b8
keerthanaaravind <keerthanaaravind8@gmail.com>
2024-01-19 18:44:39 +0530

    " Jan 19th STatic"

---
+++ src/OopsNotes/Static.java
@@ -0,0 +1,18 @@
+package src.OopsNotes;
+
+public class Static {
+
+    public static int age= 33;
+
+    public static String gender="MALE";
+
+
+    public static void Display(){
+
+        System.out.println("Customer age is "+age);
+
+        System.out.println("Customer gender is "+gender);
+    }
+}
+
+
