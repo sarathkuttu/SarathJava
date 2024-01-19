3b70bcdddff6f2a39b67b301049f8a1d1ae584f9
keerthanaaravind <keerthanaaravind8@gmail.com>
2024-01-19 18:57:59 +0530

    " Jan 19th STatic"

---
+++ src/OopsNotes/Demo_Static.java
@@ -0,0 +1,15 @@
+package src.OopsNotes;
+
+public class Demo_Static {
+
+    static {
+
+        System.out.println("STATIC is running");
+    }
+
+    {
+        System.out.println("NORMAL is running");
+    }
+
+
+}
---
+++ src/OopsNotes/Demo_Static2.java
@@ -0,0 +1,14 @@
+package src.OopsNotes;
+
+public class Demo_Static2 {
+
+    public static void main(String[] args) {
+
+        Demo_Static a = new Demo_Static();
+        Demo_Static b = new Demo_Static();
+
+
+    }
+
+
+}
