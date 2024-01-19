f417090d31d09e934710f2737837ac0201e29b38
keerthanaaravind <keerthanaaravind8@gmail.com>
2024-01-19 12:33:03 +0530

    " Jan 19th default mod"

---
+++ src/OopsNotes/Default_Modifiers_Shop.java
@@ -0,0 +1,25 @@
+package src.OopsNotes;
+
+public class Default_Modifiers_Shop {
+
+    String productname;
+
+    int productprice;
+
+    Default_Modifiers_Shop(String pname,int prodprice){
+
+        this.productname=pname;
+
+        this.productprice=prodprice;
+    }
+
+    void DISPLAY(){
+
+        System.out.println("THE PRODUCT NAME IS "+this.productname);
+        System.out.println("THE PRODUCT PRICE IS "+this.productprice);
+    }
+}
+
+
+
+
