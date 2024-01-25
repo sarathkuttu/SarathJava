dd9bb5483996bffb8e0f05cc2270f57f93da955f
keerthanaaravind <keerthanaaravind8@gmail.com>
2024-01-25 20:00:57 +0530

    "Base"

---
+++ src/Collections/List_Example.java
@@ -0,0 +1,21 @@
+package src.Collections;
+
+import java.util.List;
+
+public class List_Example {
+
+    public static void main(String[] args) {
+
+        List name = List.of("SARATH","AMMU");
+
+        List Age = List.of(33,29,3);
+
+        System.out.println(name.get(1));
+
+        System.out.println(Age.size());
+
+        System.out.println(name.isEmpty());
+
+
+    }
+}
