1d3f1277eabfeba75212ea4f76851dbf75e7388a
keerthanaaravind <keerthanaaravind8@gmail.com>
2024-01-29 23:52:18 +0530

    "Collections"

--- src/Collections/Course_List.java
+++ src/Collections/Course_List.java
@@ -1,11 +1,41 @@
 package src.Collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
 public class Course_List {
 
     public static void main(String[] args) {
 
-        List courselist = new Course_List()
+        List courselist = new ArrayList();
+
+        courselist.add("SARATH");
+        courselist.add("AMMU");
+        courselist.add("NAINIKA");
+
+        List marklist = new ArrayList();
+
+        marklist.add(99);
+        marklist.add(100);
+        marklist.add(90);
+
+        courselist.addAll(marklist);
+
+        System.out.println(courselist);
+
+        Iterator iterator = courselist.iterator();
+
+        while (iterator.hasNext()) {
+
+
+            System.out.println(iterator.next());
+
+        }
+
+        Collections.sort(marklist);
+        System.out.println(marklist);
+
     }
-
-
 }
